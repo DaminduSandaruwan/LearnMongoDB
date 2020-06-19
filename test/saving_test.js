b67cfa166b706871c01a681f74875617afcea48f
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-19 22:57:17 +0530

    Saving Data to MongoDB

---
+++ test/saving_test.js
@@ -0,0 +1,22 @@
+const mocha = require('mocha');
+const assert = require('assert');
+const MarioChar = require('../models/mariochar');
+
+//Describe tests
+describe('Saving Records',function(){
+    
+    it('Saves record to database',function(done){
+
+        var char = new MarioChar({
+            name: 'Mario'
+        });
+
+        char.save().then(function(){
+            assert(char.isNew===false);
+            done();
+        });
+
+    });
+
+
+});
