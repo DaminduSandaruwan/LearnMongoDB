496d1fc62933873483fa0fd532532e2bb95bbada
Damindu Sandaruwan <kgdsbandarak@gmail.com>
2020-06-20 14:58:32 +0530

    ES6 Promises

--- test/connection.js
+++ test/connection.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 
-// Connect to mongodb
-mongoose.connect('mongodb://localhost/testroo');
+//ES6 Promises
+mongoose.Promise=global.Promise;
 
-//once is an event listner
-mongoose.connection.once('open',function(){
-    console.log("Connection has been made");
-}).on('error',function(error){
-    console.log("Connection Error : ", error);
+//Connect to the db before tests run
+before(function(done){
+    // Connect to mongodb
+    mongoose.connect('mongodb://localhost/testroo');
+
+    //once is an event listner
+    mongoose.connection.once('open',function(){
+        console.log("Connection has been made");
+        done();
+    }).on('error',function(error){
+        console.log("Connection Error : ", error);
+    });
 });
+
